101278588fa5f5b46ec6baa4dff753b581e09d83
Nyalse <assuperas2312@gmail.com>
2024-11-21 23:44:05 +0900

    dll

---
+++ DLL/myclass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DLL
+{
+    public class myclass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Привет, Мир!");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Мир привет!");
+        }
+        public int Method3()
+        {
+            return 52; 
+        }
+
+        public string Method4()
+        {
+            return "Hello, World!"; 
+        }
+
+        public bool Method5()
+        {
+            return true; 
+        }
+
+    }
+}
