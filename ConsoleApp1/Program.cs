0af12c783d4327a0df3f4120aaa31161c1bfd941
Nyalse <assuperas2312@gmail.com>
2024-11-23 00:42:58 +0900

    study3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+<<<<<<< Updated upstream
         myclass myClass = new myclass();
         myClass.Method1();
         myClass.Method2();
@@ -22,7 +23,34 @@ internal class Program
         //user.Male=Console.ReadLine();
         //Console.WriteLine($"Я,{user.Name} {user.Surname} мне {user.Age}, и мой пол {user.Male}");
         //MyMethod1();
+=======
+        //myclass myClass = new myclass();
+        //myClass.Method1();
+        //myClass.Method2();
+        //Console.WriteLine(myClass.Method3());
+        //Console.WriteLine(myClass.Method4());
+        //Console.WriteLine(myClass.Method5()); Тут код DLL
+ 
+        //MyMethod1(); тут код методов
+>>>>>>> Stashed changes
         //MyMethod();
+
+        try { User user = new User();
+            Console.WriteLine("Ваша анкета");
+            Console.WriteLine("Введите ваше имя: ");
+            user.Name = Console.ReadLine();
+            Console.WriteLine("Введите вашу фамилию: ");
+            user.Surname = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст: ");
+            user.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ваш пол: ");
+            user.Male = Console.ReadLine();
+            Console.WriteLine($"Я,{user.Name} {user.Surname} мне {user.Age}, и мой пол {user.Male}");
+        }
+        catch(System.FormatException) { Console.WriteLine("Неверно задан возраст");
+            Console.WriteLine("Если у вас были какие то ошибки то, начните регистрацию сначала и проверьте корректность введенных данных");
+        }
+        finally { Console.WriteLine("Приятного дня!");  }
 
     }
     //public static void MyMethod()
--- DLL/myclass.cs
+++ DLL/myclass.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿using System;
 
 namespace DLL
@@ -29,4 +30,37 @@ namespace DLL
         }
 
     }
+=======
+﻿using System;
+
+namespace DLL
+{
+    public class myclass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Привет, Мир!");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Мир привет!");
+        }
+        public int Method3()
+        {
+            return 52; 
+        }
+
+        public string Method4()
+        {
+            return "Hello, World!"; 
+        }
+
+        public bool Method5()
+        {
+            return true; 
+        }
+
+    }
+>>>>>>> Stashed changes
 }
