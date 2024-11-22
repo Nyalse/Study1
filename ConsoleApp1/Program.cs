5554082d1dae6b361674105e0f55f134fd2c7dca
Nyalse <assuperas2312@gmail.com>
2024-11-22 17:58:08 +0900

    аыооаоыдвлао

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using DLL;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        myclass myClass = new myclass();
+        myClass.Method1();
+        myClass.Method2();
+        Console.WriteLine(myClass.Method3());
+        Console.WriteLine(myClass.Method4());
+        Console.WriteLine(myClass.Method5());
+        //User user = new User();
+        //Console.WriteLine("Ваша анкета");
+        //Console.WriteLine("Введите ваше имя: ");
+        //user.Name = Console.ReadLine();
+        //Console.WriteLine("Введите вашу фамилию: ");
+        //user.Surname= Console.ReadLine();
+        //Console.WriteLine("Введите ваш возраст: ");
+        //user.Age=Convert.ToInt32( Console.ReadLine());
+        //Console.WriteLine("Введите ваш пол: ");
+        //user.Male=Console.ReadLine();
+        //Console.WriteLine($"Я,{user.Name} {user.Surname} мне {user.Age}, и мой пол {user.Male}");
+        //MyMethod1();
+        //MyMethod();
+
+    }
+    //public static void MyMethod()
+    //{ Console.WriteLine("!рим,тевирП"); }
+    //private static void MyMethod1()
+    //{
+    //    Console.WriteLine("Привет,мир!");
+    //}
+    class User
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+        public string Male;
+
+    }
+}
